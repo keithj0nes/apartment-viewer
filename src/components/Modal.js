9cf54a28c9010d33db1c60663ec2d27257a5bfb0
keith <keith@raincityambience.com>
2018-03-10 00:40:50 -0800

    Show 4 items in amenities

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -17,17 +17,45 @@ const Modal = ({show, details, close}) => {
     }
   }
 
+  const a = details.amenities;
+    let amenities = {};
+    Object.keys(a).map((item, index)=>{
+      // console.log(a[item], 'itemsss');
+      if(Object.keys(amenities).length < 4){
+        if(a[item] === amenities[item]){
+          // console.log("getting herrrrree");
+        } else if(a[item] === true){
+          amenities[item] = a[item]
+        } else if(typeof a[item] === "string"){
+          amenities[item] = a[item]
+        }
+      }
+    })
+
     return (
 
       <div className="modal-bg" onClick={handleClose}>
         <div className="modal-content">
           <div className="content-left">
-            <h1>HAHAHAHAAHHA WORLD</h1>
-            <h1>HAHAHAHAAHHAasdgas fdssgasd</h1>
-
+            <div className="logo">Logo</div>
+            <p className="shortdesc">{details.shortdesc}</p>
+            <p className="address">{details.address.street} {details.address.city}, {details.address.state}, {details.address.zipcode}</p>
+            <div className="amenities">
+              {Object.keys(amenities).map((a, k)=>{
+                return <div key={k}>{a} {amenities[a] === true ? "x" : amenities[a]}</div>
+              })}
+            </div>
           </div>
           <div className="content-right">
-          
+            <img src={details.apartment} alt={details.apartment}/>
+            <div className="rent">
+              <p>Monthly Rent</p>
+              <p>{details.rent.cost}</p>
+            </div>
+            <div className="availability">
+              <p>Availability</p>
+              <p>{details.available.now === true ? "Right Now": details.available.wait}</p>
+            </div>
           </div>
 
 
